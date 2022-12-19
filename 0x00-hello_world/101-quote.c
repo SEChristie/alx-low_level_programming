f402ba7c41e22ab13d9284074eb2f59ca151731e
SEChristie <christiedike3@gmail.com>
2022-12-19 06:00:43 +0100

    UNIX is basically a simple OS

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - print quote in C
+ * Return:0
+*/
+int main(void)
+{
+	write(2, "and that piece of art is useful\"- Dora Korpar, 2015-10-19\n", 59);
+	return (1);
+}
