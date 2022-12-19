2d1d491b71aeafe428ac3e71b8b9e810602d354f
SEChristie <christiedike3@gmail.com>
2022-12-19 07:43:03 +0100

    UNIX is basically a simple OS

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,10 @@
+#include <stdio.h>
 #include <unistd.h>
 /**
- * main - prints exactly and that piece of art is useful
- * Return:0
+ * main - prints exactly "and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19",
+ * followed by a new line, to the standard error.
+ * Return: Always 0 (Success)
 */
 int main(void)
 {
