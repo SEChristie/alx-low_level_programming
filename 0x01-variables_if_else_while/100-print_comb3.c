a1a29679da87ca258fe3bc5a6910015518cf894e
SEChristie <christiedike3@gmail.com>
2022-12-20 21:39:34 +0100

    Inventing is a combination

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print all possible different combinations of two digits
+ * in ascending order, separated by a coma followed by a space
+ * Return: (0) Always
+*/
+int main(void)
+{
+	int digit1, digit2;
+
+	for (digit1 = 0; digit1 < 9; digit1++)
+	{
+		for (digit2 = digit1 + 1; digit2 < 10; digit2++)
+		{
+			putchar((digit1 % 10) + '0');
+			putchar((digit2 % 10) + '0');
+
+			if (digit1 == 8 && digit2 == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
+		}
+	}
+
+	putchar('\n');
+	return (0);
+}
