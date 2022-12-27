1a8a748368affde64442b0269ac3401934edaef4
SEChristie <christiedike3@gmail.com>
2022-12-27 20:01:55 +0100

    Swap horses

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * swap_int - write a function that swaps the values of two integers
+ * @a: the variable pointer at a
+ * @b: the variable pointer at b
+*/
+
+void swap_int(int *a, int *b)
+{
+	int i = *a;
+	*a = *b;
+	*a = i;
+}
