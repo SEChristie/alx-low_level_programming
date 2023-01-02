da7d8b546ba938bd689473d355a73ff10c536c2c
SEChristie <christiedike3@gmail.com>
2023-01-02 08:29:07 +0100

    Addition

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -14,12 +14,12 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 	int i, j, k, l, m, n;
 
 	for (i = 0; n1[i]; i++)
-	;
+
 	for (j = 0; n2[j]; j++)
-	;
+
 	if (i > size_r || j > size_r)
-	return (0);
-	m = 0;
+		return (0);
+		m = 0;
 	for (i -= 1, j -= 1, k = 0; k < size_r - 1; i--, j--, k++)
 	{
 		n = m;
@@ -31,10 +31,10 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		{
 			break;
 		}
-		m = n / 10;
-		r[k] = n % 10 + '0';
+			m = n / 10;
+			r[k] = n % 10 + '0';
 	}
-	r[k] = '\0';
+		r[k] = '\0';
 	if (i >= 0 || j >= 0 || m)
 		return (0);
 	for (k -= 1, l = 0; l < k; k--, l++)
