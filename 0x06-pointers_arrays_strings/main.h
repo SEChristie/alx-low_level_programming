1cd978e2d0deceb598b2129b4aa8588e757ff7e5
SEChristie <christiedike3@gmail.com>
2023-01-02 02:29:10 +0100

    Numbers have life

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -14,5 +14,7 @@ char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 void print_uint(unsigned int n);
+nt _putchar(char c);
+
 
 #endif
