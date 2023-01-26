36c52f686b46350dc3c48027826f48f4474aa17e
SEChristie <christiedike3@gmail.com>
2023-01-26 13:04:00 +0100

    Name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - prints a name depends of the function.
+ * @name: pointer to Name's string.
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
