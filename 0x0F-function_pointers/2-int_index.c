18860fce41e06d8cc8342b8def0b9e3354c4b806
SEChristie <christiedike3@gmail.com>
2023-01-26 15:29:10 +0100

    Hell

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -4,7 +4,7 @@
  * int_index - a function that searches for an integer
  * @array: array to search in
  * @size: size of the array
- * @cmp: pointer to the comparing function 
+ * @cmp: pointer to the comparing function
  * Return: index of the first element for which
  * the cmp function does not return 0, or -1 if no match is found
  * or size is negative
