e74be83bb698983c7d7c0fc725f57b9aa62c9036
SEChristie <christiedike3@gmail.com>
2023-02-10 21:27:33 +0100

    Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,54 +1,33 @@
-#include <unistd.h>
-#include <stdlib.h>
-#include <fcntl.h>
 #include "main.h"
 
 /**
  * read_textfile - reads and prints from a file
+ * prints it to the POSIX standard output.
  * @filename: path to file
- * @letters: chars to read
- * Return: chars read
+ * @letters: the numbers of letters it should read and print.
+ * Return: the actual number of letters it could read and print
 */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	char *buff;
-	ssize_t bytes, r;
+	char *print_field;
+	int file, read_file;
 
 	if (!filename)
 		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-	{
-		close(fd);
+
+	print_field = malloc(letters * sizeof(char));
+	if (!print_field)
 		return (0);
-	}
 
-	buff = malloc(sizeof(char) * letters);
-	if (!buff)
-	{
-		close(fd);
+	file = open(filename, O_RDONLY);
+	if (file == -1)
 		return (0);
-	}
 
-	bytes = read(fd, buff, letters);
+	read_file = read(file, print_field, letters);
+	write(STDOUT_FILENO, print_field, read_file);
 
-	if (bytes == -1)
-	{
-		close(fd);
-		free(buff);
-		return (0);
-	}
-
-	r = write(STDOUT_FILENO, buff, bytes);
-
-	if (r == -1)
-	{
-		close(fd);
-		free(buff);
-		return (0);
-	}
-	close(fd);
-	close(fd);
+	close(file);
+	free(print_field);
+	return (read_file);
 }
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -1,11 +1,13 @@
-#ifndef IO_
-#define IO_
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
+#include <sys/types.h>
+#include <sys/stat.h>
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
 
-int _putchar(char c);
+int _putchar(char);
 int _strlen(const char *s);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
